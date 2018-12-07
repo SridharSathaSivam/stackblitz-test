b61058ccac6b594bc9809353a4f662b3597bfe36
ROOT\kumaresan.subramani <kumaresan.subramani@syncfusion.com>
2018-12-07 10:16:25 +0530

    commit sample

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+
+import {
+  NgModule, Component, Pipe, PipeTransform, OnInit, Input, IterableDiffers, DoCheck,
+  ViewChild, ChangeDetectorRef
+} from '@angular/core';
+
+
+@Component({
+  selector: 'app-my',
+  template: `
+  <img src="./assets/wow.jpg"/>
+ `
+})
+export class AppComponent {
+
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
